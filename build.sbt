09a516d370f0ce7a816307d9c701d4fa7587b67c
Philip Wills <philip.wills@guardian.co.uk>
2012-05-08 15:17:47 +0100

    Basic retrieval of EC2 hosts for use by Magenta

---
+++ build.sbt
@@ -0,0 +1,13 @@
+organization := "com.gu"
+
+name := "ec2-host-provider"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+libraryDependencies ++= Seq(
+  "com.amazonaws" % "aws-java-sdk" % "1.3.4",
+  "net.liftweb" %% "lift-json" % "2.4"
+)
+
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,48 @@
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.ec2.model.{InstanceState, InstanceStateName, Instance}
+import com.amazonaws.services.ec2.{AmazonEC2Client, AmazonEC2}
+import collection.JavaConversions._
+import net.liftweb.json.{NoTypeHints, Serialization}
+
+object Main extends App {
+  implicit val formats = Serialization.formats(NoTypeHints)
+  print(Serialization.write(new EC2HostProvider().hosts))
+}
+
+class EC2HostProvider extends EC2 {
+  def instanceResponse = ec2.describeInstances
+
+  def tagValue(instance: Instance, key: String) = instance.getTags.find(_.getKey == key).map(_.getValue).getOrElse("")
+
+  def hosts = {
+    for {
+      res <- instanceResponse.getReservations
+      instance <- res.getInstances if instance.getState.getName == InstanceStateName.Running.toString
+    } yield {
+      DeployInfoHost(instance.getPublicDnsName, app = tagValue(instance, "App"), stage = tagValue(instance, "Stage"))
+    }
+  }
+}
+
+case class DeployInfoHost(
+  hostname: String,
+  app: String,
+  group: String = "",
+  stage: String
+)
+
+
+trait EC2 extends AWS {
+  lazy val ec2 : AmazonEC2 = new AmazonEC2Client(credentials)
+  ec2.setEndpoint("ec2.eu-west-1.amazonaws.com")
+}
+
+trait AWS {
+  lazy val accessKey = Option(System.getenv.get("aws_access_key")).getOrElse{
+    sys.error("Cannot authenticate, 'aws_access_key' must be set as an environment variable")
+  }
+  lazy val secretAccessKey = Option(System.getenv.get("aws_secret_access_key")).getOrElse{
+    sys.error("Cannot authenticate, aws_secret_access_key' must be set as an environment variable")
+  }
+  lazy val credentials = new BasicAWSCredentials(accessKey, secretAccessKey)
+}
